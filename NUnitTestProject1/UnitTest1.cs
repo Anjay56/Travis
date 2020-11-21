ca92eb31289b53f17cb879ac7ef60dd77db451bd
Anjay56 <101229576@students.swinburne.edu.my>
2020-11-21 12:32:18 +0700

    Update Travis (NUnit Part 1)

---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using TestingTravis;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Form1 form = new Form1();
+            Assert.AreEqual(4, form.getY());
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Form1 form = new Form1();
+            Assert.AreEqual(3, form.getX());
+        }
+    }
+}
