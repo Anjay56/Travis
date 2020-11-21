3a89d0a46871726b8f1386fe92f020b4ca853746
Anjay56 <101229576@students.swinburne.edu.my>
2020-11-21 16:35:06 +0700

    hjkl

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -14,15 +14,16 @@ namespace NUnitTestProject1
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Form1 form = new Form1();
+            Assert.AreEqual(4, form.getY());
         }
-        /*
+        
         [Test]
         public void Test2()
         {
             Form1 form = new Form1();
             Assert.AreEqual(3, form.getX());
         }
-        */
+        
     }
 }
